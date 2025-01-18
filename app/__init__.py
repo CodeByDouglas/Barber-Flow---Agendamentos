e55d9a0e02b4aa0965e28049728116d0e0b37b30
DougDev <douglas.ofc.2004@gmail.com>
2025-01-18 04:26:12 +0000

    Configura o app Flask no `__init__.py` e inicializa o servidor no `run.py`

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask 
+from .routes import rotas_de_adm, rotas_de_agendamento
+
+
+def create_app(): 
+    app = Flask(__name__)
+    app.config.from_object('config')
+
+    app.register_blueprint(rotas_de_agendamento.bp)
+    app.register_blueprint(rotas_de_adm.bp)
+
+    return app
