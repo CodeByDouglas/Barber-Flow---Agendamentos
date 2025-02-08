fb501c455a8863f80b1c1295dbfde23989c4ce6c
DougDev <douglas.ofc.2004@gmail.com>
2025-02-08 21:35:08 +0000

    `feat(routes):` redireciona a URL raiz para /agenda

--- app/controllers/routes/rotas_de_agendamento.py
+++ app/controllers/routes/rotas_de_agendamento.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 bp = Blueprint('agenda', __name__)
+
+@bp.route('/')
+def index():
+    return redirect(url_for('agenda.tela_inicial'))
 
 @bp.route('/agenda')
 def tela_inicial():
