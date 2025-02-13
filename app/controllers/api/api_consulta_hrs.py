c3a4ac854ad4c4726e79cd4abf056a627dad3423
DougDev <douglas.ofc.2004@gmail.com>
2025-02-13 23:27:39 +0000

    Refatora os endpoints de `consulta_hrs` e `create_agendamentos`

--- app/controllers/api/api_consulta_hrs.py
+++ app/controllers/api/api_consulta_hrs.py
@@ -3,13 +3,17 @@ from app.utils.consulta_hrs import consultar_horarios
 
 bp_api = Blueprint('api_consulta_hrs', __name__, url_prefix='/api')
 
-@bp_api.route('/consulta_hrs', methods=['GET'])
+@bp_api.route('/consulta_hrs', methods=['POST'])
 def consulta_hrs():
-    id_funcionario = request.args.get('id', type=int)
-    dia = request.args.get('dia', type=int)
-    mes = request.args.get('mes', type=int)
-    ano = request.args.get('ano', type=int)
-    admin_id = request.args.get('admin_id')
+    data = request.get_json()
+    if not data:
+        return jsonify({'error': 'JSON body is required.'}), 400
+
+    id_funcionario = data.get('id')
+    dia = data.get('dia')
+    mes = data.get('mes')
+    ano = data.get('ano')
+    admin_id = data.get('admin_id')
 
     if None in (id_funcionario, dia, mes, ano, admin_id):
         return jsonify({'error': 'Os parâmetros "id", "dia", "mes", "ano" e "admin_id" são necessários.'}), 400
